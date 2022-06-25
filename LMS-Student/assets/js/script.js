e2e4155c3617e84cb9592437ffcbb4fa3456874e
Alexander Saga <lexansaga72@gmail.com>
2022-06-25 23:01:00 +0800

    Updates
    Module design added

--- LMS-Student/assets/js/script.js
+++ LMS-Student/assets/js/script.js
@@ -1,4 +1,6 @@
 $(window).on('load', function () {
+    // DataTable
+
     $('#datatable').DataTable({
         ordering: true,
         pageLength: 20,
@@ -8,7 +10,27 @@ $(window).on('load', function () {
         ]
     });
 
+    //Menu Bar 
+
+    var isMenuOpen = true;
     $('.menu').on('click', function () {
         $('.sidebar').toggleClass('open')
+
+        if (isMenuOpen == true) {
+            $('.menu').empty().html(' <i class="bx bx-x"></i>')
+            isMenuOpen = false;
+        } else {
+            $('.menu').empty().html(' <i class="bx bx-menu"></i>')
+            isMenuOpen = true;
+        }
+        console.log(isMenuOpen)
     })
+
+
+    // Calendar Script
+    // Link : https://www.jqueryscript.net/time-clock/animated-event-calendar.html
+    $(function () {
+        $('#calendar-container').calendar();
+    });
+
 })
